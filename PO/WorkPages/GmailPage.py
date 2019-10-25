ad8372c619641f89951ff58da1f7480043e494ab
infernolisarum <infernolisarum@gmail.com>
2019-10-26 02:11:15 +0300

    third commit

--- PO/WorkPages/GmailPage.py
+++ PO/WorkPages/GmailPage.py
@@ -1,5 +1,4 @@
-import time
-
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as expected
@@ -7,33 +6,43 @@ from PO.BasePage import BasePage
 
 
 class GmailPage(BasePage):
-
     def init_page(self):
         self.driver.get("https://mail.google.com/mail/?tab=wm&amp;ogbl")
 
     def login(self, login, password):
-        self.driver.find_element(By.XPATH, "//input[@type='email']").send_keys(login)
-        self.driver.find_element(By.XPATH, "//span[@class='RveJvd snByac']").click()
-        WebDriverWait(self.driver, 10).until(expected.element_to_be_clickable((By.XPATH, "//input[@type='password']")))
-        self.driver.find_element(By.XPATH, "//input[@type='password']").send_keys(password)
-        self.driver.find_element(By.XPATH, "//span[@class='RveJvd snByac']").click()
+        email_field_locator = (By.XPATH, "//input[@type='email']")
+        login_button_locator = (By.XPATH, "//span[@class='RveJvd snByac']")
+        password_field_locator = (By.XPATH, "//input[@type='password']")
+        WebDriverWait(self.driver, 10).until(expected.visibility_of_element_located(
+            email_field_locator)).send_keys(login)
+        WebDriverWait(self.driver, 10).until(expected.element_to_be_clickable(
+            login_button_locator)).click()
+        WebDriverWait(self.driver, 10).until(expected.visibility_of_element_located(
+            password_field_locator)).send_keys(password)
+        WebDriverWait(self.driver, 10).until(expected.element_to_be_clickable(
+            login_button_locator)).click()
 
-    def send_letter(self, to_whom, subject_message, text_massage):
+    def send_letter(self, address, subject_message, text_massage):
+        new_letter_button_locator = (By.XPATH, "//div[@class='T-I J-J5-Ji T-I-KE L3']")
+        address_field_locator = (By.XPATH, "//textarea[@rows='1']")
+        subjectbox_locator = (By.XPATH, "//input[@name='subjectbox']")
+        textbox_locator = (By.XPATH, "//div[@role='textbox']")
+        send_letter_locator = (By.XPATH, "//div[@class='dC']")
         WebDriverWait(self.driver, 10).until(
-            expected.element_to_be_clickable((By.XPATH, "//div[@class='T-I J-J5-Ji T-I-KE L3']"))).click()
-        WebDriverWait(self.driver, 10).until(expected.element_to_be_clickable((By.XPATH, "//div[@class='dC']")))
-        self.driver.find_element(By.XPATH, "//textarea[@rows='1']").send_keys(to_whom)
-        self.driver.find_element(By.XPATH, "//input[@name='subjectbox']").send_keys(subject_message)
-        self.driver.find_element(By.XPATH, "//div[@role='textbox']").send_keys(text_massage)
-        WebDriverWait(self.driver, 10).until(expected.element_to_be_clickable((By.XPATH, "//div[@class='dC']"))).click()
+            expected.element_to_be_clickable(new_letter_button_locator)).click()
+        WebDriverWait(self.driver, 7).until(expected.visibility_of_element_located(address_field_locator))\
+            .send_keys(address)
+        WebDriverWait(self.driver, 7).until(expected.visibility_of_element_located(subjectbox_locator))\
+            .send_keys(subject_message)
+        WebDriverWait(self.driver, 7).until(expected.visibility_of_element_located(textbox_locator))\
+            .send_keys(text_massage)
+        WebDriverWait(self.driver, 10).until(expected.element_to_be_clickable(send_letter_locator)).click()
 
-    def refresh_mail(self):
-        locator = (By.XPATH, "//div[@class='G-Ni J-J5-Ji']")  # TODO: change to correct name
-        WebDriverWait(self.driver, 10).until(
-            expected.element_to_be_clickable(locator)).click()
-        WebDriverWait(self.driver, 10).until(
-            expected.element_to_be_clickable(locator))  # TODO: change to 'letters.element_to_be_selected()...
+    def refresh_letters(self):
+        refresh_button_locator = (By.XPATH, "//div[@class='T-I J-J5-Ji nu T-I-ax7 L3' and @role='button']")
+        WebDriverWait(self.driver, 5).until(expected.visibility_of_element_located(refresh_button_locator)).click()
 
     def get_all_letters(self):
-        letters = self.driver.find_elements(By.XPATH, "//div/div/table/tbody/tr[@jsmodel='nXDxbd']")
+        all_letters_locator = (By.XPATH, "//div/div/table/tbody/tr[@jsmodel='nXDxbd']")
+        letters = WebDriverWait(self.driver, 5).until(expected.presence_of_all_elements_located(all_letters_locator))
         return letters
--- Tests/GmailTests/GmailSteps.py
+++ Tests/GmailTests/GmailSteps.py
@@ -1,7 +1,6 @@
 import random
 import string
 
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as expected
 from selenium.webdriver.support.wait import WebDriverWait
@@ -28,61 +27,80 @@ def character_counter(string_to_count):
 
 
 class GmailSteps(BaseStep):
+    number_letters = 15
+    mail_address = "testforasferro@gmail.com"
+    result_letter_subject = "Result letter"
+    message_subject_locator = ".//td/div/div/div/span/span[@class='bqe']"
+    message_text_locator = ".//td/div/div/span[@class='y2']"
+
     def __init__(self, driver):
         super(GmailSteps, self).__init__(driver)
-        self.number_letters = 15
-        self.my_dict_for_letters = {}
-        self.mail_address = "testforasferro@gmail.com"
+        self.dict_for_letters = {}
         self.page = GmailPage(self.driver)
         self.start_steps()
-        self.send_letters_to_yourself()
-        self.select_text_from_my_letters()
+        self.send_package_letters_to_yourself()
+        self.select_text_from_package_letters()
         self.send_result_letter_with_report()
+        self.delete_all_letters_except_last()
         self.finish_tests()
 
     def start_steps(self):
         self.page.init_page()
         self.page.login("testforasferro", "TestForAsferro1")
 
-    def send_letters_to_yourself(self):
+    def send_package_letters_to_yourself(self):
         for i in range(self.number_letters):
-            self.page.send_letter(self.mail_address, create_random_string(), create_random_string())
-            self.page.refresh_mail()
+            temp_subject, temp_text = "", ""
+            temp_subject += create_random_string()
+            temp_text += create_random_string()
+            self.page.send_letter(self.mail_address, temp_subject, temp_text)
+            self.check_sending_letter(temp_subject, temp_text)
 
-    # TODO: after 'for' => 'if current_number_letters < 15:'...
-    def check_letters_in_mail(self):
-        current_number_letters = 0
+    def check_sending_letter(self, subject, text):
+        WebDriverWait(self.page.driver, 7).until(expected.text_to_be_present_in_element(
+            (By.XPATH, self.message_subject_locator), subject))
+        WebDriverWait(self.page.driver, 7).until(expected.text_to_be_present_in_element(
+            (By.XPATH, self.message_text_locator), text))
+
+    def select_text_from_package_letters(self):
+        from_who_address_locator = ".//td/div/span/span[@email]"
         verified_letters = []
         letters = self.page.get_all_letters()
         for i in range(len(letters)):
-            from_whom = str(letters[i].find_element_by_xpath(".//td/div/span/span[@email]").get_attribute("email"))
-            if self.mail_address in from_whom:
+            if i > 15:
+                break
+            from_who = str(letters[i].find_element_by_xpath(from_who_address_locator).get_attribute("email"))
+            if self.mail_address in from_who:
                 verified_letters.append(letters[i])
-                current_number_letters += 1
-            elif current_number_letters < self.number_letters:
-                assert "Found {current_number_letters} letters of {number_letters} sent letters."
-        return verified_letters
-
-    def select_text_from_my_letters(self):
-        message_subject_locator = ".//td/div/div/div/span/span[@class='bqe']"
-        message_text_locator = ".//td/div/div/span[@class='y2']"
-        self.page.refresh_mail()
-        letters = self.check_letters_in_mail()
-        for i in range(len(letters)):
-            message_subject = str(letters[i].find_element_by_xpath(message_subject_locator).text)
-            message_text = str(letters[i].find_element_by_xpath(message_text_locator).text)
+        for i in range(len(verified_letters)):
+            message_subject = str(verified_letters[i].find_element_by_xpath(self.message_subject_locator).text)
+            message_text = str(verified_letters[i].find_element_by_xpath(self.message_text_locator).text)
             format_message_text = message_text.replace(" - \n", "")
-            self.my_dict_for_letters[message_subject] = format_message_text
+            self.dict_for_letters[message_subject] = format_message_text
 
     def send_result_letter_with_report(self):
         result_string = ""
-        for key, value in self.my_dict_for_letters.iteritems():
+        for key, value in self.dict_for_letters.iteritems():
             char_counter = character_counter(value)
             report_format = "Received mail on theme '{0}' with message: '{1}'. It contains '{2}' letters and '{3}' " \
                             "numbers".format(key, value, char_counter, 10 - char_counter)
             result_string += (report_format + "\n")
-        self.page.send_letter(self.mail_address, "Result letter", result_string)
-        self.page.refresh_mail()
+        self.page.send_letter(self.mail_address, self.result_letter_subject, result_string)
+        self.page.refresh_letters()
 
+    def delete_all_letters_except_last(self):
+        select_all_letters_locator = (By.XPATH, "//div/div/div/span[@role='checkbox']")
+        checkbox_any_letter_locator = ".//tr/td/div[@role='checkbox']"
+        delete_button_locator = (By.XPATH, ".//div/div/div[@role='button' and @data-tooltip='Delete']")
+        WebDriverWait(self.page.driver, 7).until(expected.element_to_be_clickable(
+            select_all_letters_locator)).click()
+        WebDriverWait(self.page.driver, 7).until(expected.element_to_be_clickable(
+            (By.XPATH, checkbox_any_letter_locator))).click()
+        WebDriverWait(self.page.driver, 7).until(expected.element_to_be_clickable(delete_button_locator)).click()
+        self.page.refresh_letters()
+        WebDriverWait(self.page.driver, 7).until(expected.element_to_be_clickable(
+            (By.XPATH, checkbox_any_letter_locator)))
+
+    # TODO: move to BaseStep
     def finish_tests(self):
         self.page.quit_driver()
